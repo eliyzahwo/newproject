cb0e3285d1c135866f8bc11af7d6805d28f0dec8
John Rodjers <161562774+eliyzahwo@users.noreply.github.com>
2024-02-28 08:37:29 +0200

    Create code.cs

---
+++ code.cs
@@ -0,0 +1,36 @@
+  if (GitHubActions != null)
+        {
+            Log.Debug("Add Version Postfix if under CI - GithubAction(s)...");
+
+            var buildNumber = GitHubActions.RunNumber;
+
+            if (ScheduledTargets.Contains(Default))
+            {
+                Version = $"{Version}-ci.{buildNumber}";
+            }
+            else if (ScheduledTargets.Contains(PrePublish))
+            {
+                Version = $"{Version}-alpha.{buildNumber}";
+            }
+        }
+
+  Target ElectronizeGenericTargetSample => _ => _
+        .DependsOn(CompileSample)
+        .Executes(() =>
+        {
+            var sample = SourceDirectory / DemoTargetLibName;
+            var cli = SourceDirectory / CliTargetLibName / $"{CliTargetLibName}.csproj";
+            var args = "build /target custom win7-x86;win /dotnet-configuration Debug /electron-arch ia32  /electron-params \"--publish never\"";
+
+            DotNet($"run --project {cli} -- {args}", sample);
+        });
+ Target ElectronizeWindowsTargetSample => _ => _
+        .DependsOn(CompileSample)
+        .Executes(() =>
+        {
+            var sample = SourceDirectory / DemoTargetLibName;
+            var cli = SourceDirectory / CliTargetLibName / $"{CliTargetLibName}.csproj";
+            var args = "build /target win /electron-params \"--publish never\"";
+
+            DotNet($"run --project {cli} -- {args}", sample);
+        });
